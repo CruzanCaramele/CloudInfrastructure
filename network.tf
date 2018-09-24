6bf44dbeccd2052ad5f55a2ebc5f5aa96a870f57
CruzanCaramele <letters2hamza@gmail.com>
2018-09-24 23:19:58 +0800

    add bastion subnets and route table associations

---
+++ network.tf
@@ -0,0 +1,77 @@
+#---------------------------------------------#
+# VPC #
+#---------------------------------------------#
+resource "aws_vpc" "otto_vpc" {
+  cidr_block           = "${var.vpc_cidr_block}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+#---------------------------------------------#
+# Public Subnet A #
+#---------------------------------------------#
+resource "aws_subnet" "public_webserver_zone_a" {
+  vpc_id            = "${aws_vpc.otto_vpc.id}"
+  cidr_block        = "${var.public_subnet_server_a_cidr}"
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "Public Web Server Zone A"
+  }
+}
+
+resource "aws_route_table_association" "public_webserver_zone_a" {
+    subet_id = "${aws_subnet.public_webserver_zone_a.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+#---------------------------------------------#
+# Public Subnet B #
+#---------------------------------------------#
+resource "aws_subnet" "public_webserver_zone_b" {
+  vpc_id            = "${aws_vpc.otto_vpc.id}"
+  cidr_block        = "${var.public_subnet_server_b_cidr}"
+  availability_zone = "us-east-1b"
+
+  tags {
+    Name = "Public Web Server Zone B"
+  }
+}
+
+resource "aws_route_table_association" "public_webserver_zone_b" {
+    subet_id = "${aws_subnet.public_webserver_zone_b.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+#---------------------------------------------#
+# Bastion Hosts Subnets #
+#---------------------------------------------#
+resource "aws_subnet" "public_bastion_zone_a" {
+  vpc_id            = "${aws_vpc.otto_vpc.id}"
+  cidr_block        = "${var.public_subnet_bastion_a_cidr}"
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "Public Bastion Server Zone A"
+  }
+}
+
+resource "aws_route_table_association" "public_webserver_zone_b" {
+    subet_id = "${aws_subnet.public_bastion_zone_a}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_subnet" "public_bastion_zone_b" {
+  vpc_id            = "${aws_vpc.otto_vpc.id}"
+  cidr_block        = "${var.public_subnet_bastion_b_cidr}"
+  availability_zone = "us-east-1b"
+
+  tags {
+    Name = "Public Bastion Server Zone B"
+  }
+}
+
+resource "aws_route_table_association" "public_webserver_zone_b" {
+    subet_id = "${aws_subnet.public_bastion_zone_b}"
+    route_table_id = "${aws_route_table.public.id}"
+}
