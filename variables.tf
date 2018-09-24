b2e85730b7c092ab9cc9c7c873b9ec0cd7f10203
CruzanCaramele <letters2hamza@gmail.com>
2018-09-24 23:54:55 +0800

    add variables for db subnets

--- variables.tf
+++ variables.tf
@@ -16,18 +16,40 @@ variable "secret_key" {
   default     = ""
 }
 
-
+#---------------------------------------------#
+# Subnets variables #
+#---------------------------------------------#
 variable "vpc_cidr_block" {
   description = "cidr range for vpc"
-  default = ""
+  default     = ""
 }
 
 variable "public_subnet_server_a_cidr" {
   description = "subnet a cidr range"
-  default = ""
+  default     = ""
 }
 
 variable "public_subnet_server_b_cidr" {
   description = "subnet b cidr range"
-  default = ""
+  default     = ""
+}
+
+variable "public_subnet_bastion_a_cidr" {
+  description = "bastion a cidr"
+  default     = ""
+}
+
+variable "public_subnet_bastion_b_cidr" {
+  description = "bastion a cidr"
+  default     = ""
+}
+
+variable "private_subnet_db_a_cidr" {
+  description = "database cidr subnet a"
+  default     = ""
+}
+
+variable "private_subnet_db_b_cidr" {
+  description = "database cidr subnet b"
+  default     = ""
 }
